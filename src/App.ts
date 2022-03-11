75d7701669ef578c63139a3e36bc77fc853ec207
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-03-12 02:42:01 +0530

    feat: Connect to MongoDB.

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,12 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
+import mongoose from 'mongoose';
 
 import Fields from './Routes/Fields';
 import Upload from './Routes/Upload';
-import {port} from "./Config";
+import {atlasUri, port} from "./Config";
 
 const app = express();
 const storage: multer.StorageEngine = multer.memoryStorage();
@@ -12,6 +14,7 @@ const uploader: multer.Multer = multer({storage: storage});
 
 app.use(cors());
 app.use(express.json());
+mongoose.connect(atlasUri);
 
 app.get('/fields', Fields.get);
 app.post('/fields', Fields.post);
