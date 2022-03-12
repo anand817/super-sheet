075dee2b710fa4da9b86bd0fab1977774d6984fc
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-03-13 04:07:42 +0530

    feat: Add create project endpoint.

--- src/Routes/Projects.ts
+++ src/Routes/Projects.ts
@@ -5,6 +5,7 @@ import {Readable} from 'stream';
 import Buffer from 'buffer';
 import mongodb from "mongodb";
 import mongoose from "mongoose";
+import {Field} from "../Models/Field";
 
 export default class Projects {
     /**
@@ -48,21 +49,46 @@ export default class Projects {
                 status: 'File is not uploaded',
             });
         } else {
-            const tableData = await Projects.parseCsv(request.file!.buffer);
-            // TODO(BURG3R5): Store raw data temporarily
+            const projectName: string = request.body.name;
+            const {headers, rows} = await Projects.parseCsv(request.file!.buffer);
+
+            let dataCollection, fieldsCollection;
+            try {
+                dataCollection = await (<mongoose.Mongoose>global.mongooseClient).connection.db.createCollection(`${projectName}.data`);
+                fieldsCollection = await (<mongoose.Mongoose>global.mongooseClient).connection.db.createCollection(`${projectName}.fields`);
+            } catch (e) {
+                dataCollection = await (<mongoose.Mongoose>global.mongooseClient).connection.db.collection(`${projectName}.data`);
+                fieldsCollection = await (<mongoose.Mongoose>global.mongooseClient).connection.db.collection(`${projectName}.fields`);
+            }
+            let fields: Array<Field> = [];
+            for (let i = 0; i < headers.length; i++) {
+                fields.push({
+                    fieldSlug: headers[i].toLowerCase(),
+                    type: {
+                        "typeSlug": "string",
+                        "maxLength": 1000
+                    },
+                    displayName: headers[i],
+                });
+            }
+            await dataCollection.insertMany(rows);
+            await fieldsCollection.insertMany(fields);
             response.end();
         }
     }
 
-    private static async parseCsv(buffer: Buffer): Promise<{ data: Array<object> }> {
-        const users: Array<object> = [];
+    private static async parseCsv(buffer: Buffer):
+        Promise<{ headers: Array<string>; rows: Array<object> }> {
+        let headers: Array<string> = [];
+        const rows: Array<object> = [];
         const stream = Readable
             .from(buffer)
             .pipe(csv())
-            .on('data', (data) => users.push(data));
+            .on('headers', (h) => headers = h)
+            .on('data', (data) => rows.push(data));
         await new Promise<void>((resolve, _) => {
             stream.on('end', () => resolve());
         });
-        return {data: users};
+        return {headers: headers, rows: rows};
     }
 }
